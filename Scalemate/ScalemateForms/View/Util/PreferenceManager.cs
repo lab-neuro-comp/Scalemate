a62eab7b48a752a4211905bb89e2cea0d9c15b05
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-04-08 16:29:27 -0300

    Sketching preference manager

---
+++ Scalemate/ScalemateForms/View/Util/PreferenceManager.cs
@@ -0,0 +1,53 @@
+﻿using ScalemateForms.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScalemateForms.View.Util
+{
+    /// <summary>
+    /// This is the class that will handle preferences amongst the view.
+    /// </summary>
+    public class PreferenceManager
+    {
+        /// <summary>
+        /// Creates a new preference manager. Requires the existence of a 
+        /// "assets\preferences.csv" file containing all stored preferences.
+        /// </summary>
+        public PreferenceManager()
+        {
+            Preferences = new Dictionary<string, string>();
+            var rawPreferences = DAL.Load(@"assets\preferences.csv");
+            foreach (string rawPreference in rawPreferences)
+            {
+                var fields = rawPreference.Trim().Split('\t');
+                Preferences[fields[0]] = fields[1];
+            }
+        }
+
+        /// <summary>
+        /// Gets the current set language.
+        /// </summary>
+        /// <returns>The language code</returns>
+        public string GetLanguage()
+        {
+            return Preferences["language"];
+        }
+
+        /// <summary>
+        /// Stores the current preferences.
+        /// </summary>
+        public void Store()
+        {
+            DAL.Save(@"assets\preferences.csv", 
+                     Preferences.Keys
+                                .Select(k => $"{k}\t{Preferences[k]}")
+                                .Aggregate("", (box, it) => $"{box}{it}\n"));
+        }
+
+        private DataAccessLayer DAL;
+        public Dictionary<string, string> Preferences { get; private set; }
+    }
+}
